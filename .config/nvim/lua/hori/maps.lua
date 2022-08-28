10afb30ca9e45f758f090805f318aee028b7dc64
Nhat Tran <tranminhnhat2311@gmail.com>
2022-08-28 18:10:43 +0700

    Update maps.lua

--- .config/nvim/lua/hori/maps.lua
+++ .config/nvim/lua/hori/maps.lua
@@ -1,3 +1,4 @@
+vim.g.mapleader = ","
 local keymap = vim.keymap
 
 keymap.set('i', 'jj', '<Esc>')
@@ -18,14 +19,23 @@ keymap.set('n', 'dw', 'vb"_d')
 -- Select all
 keymap.set('n', '<C-a>', 'gg<S-v>G')
 
+-- Copy and paste with system clipboard
+
+vim.keymap.set("v", "<C-c>", '"+y', { silent = true })
+vim.keymap.set("i", "<C-v>", "<C-r>+", { silent = true })
+
+-- Save and quit
+
+vim.keymap.set("n", "<C-s>", ":write<cr>", { silent = true })
+vim.keymap.set("n", "<C-q>", ":quit<cr>", { silent = true })
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 
 -- New tab
-keymap.set('n', 'te', ':tabedit')
-keymap.set('n', 'J', ':tabprev')
-keymap.set('n', 'K', ':tabnext')
+keymap.set('n', 'te', ':tabedit<cr>')
+keymap.set('n', 'J', ':tabprev<cr>')
+keymap.set('n', 'K', ':tabnext<cr>')
 keymap.set('n', '<leader>1', '1gt')
 keymap.set('n', '<leader>2', '2gt')
 keymap.set('n', '<leader>3', '3gt')
@@ -55,8 +65,13 @@ keymap.set('n', '<C-w><right>', '<C-w>>')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
 
--- Terminal windows
+-- Terminal windows  :PackerLoad toggleterm.lua<cr>:ToggleTerm<cr>
+keymap.set('n', "<C-m>", ":echo 'wrong key'<CR>")
 -- keymap.set('t', '<Esc>', '<C-\><C-n>')
 -- keymap.set('t', ':q!', '<C-\><C-n>:q!<CR>')
 
+-- Nvim Tree
+keymap.set("n", "<leader>d", ":PackerLoad nvim-tree.lua<cr>:NvimTreeToggle<CR>", { silent = true })
+
+keymap.set("n", "<S-q>", ":PackerLoad nvim-tree.lua<cr>:NvimTreeFocus<CR>", { silent = true })
 
