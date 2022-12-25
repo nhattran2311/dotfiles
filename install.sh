4d919941beb2ef5bd791a4314b81b5ae618ae4d3
Nhat Tran <tranminhnhat2311@gmail.com>
2022-12-25 19:53:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-#!bin/sh
+#!bin/bash
+# dotfile installer
 #TODO: need make this script work
 printGreen() {
   echo "\e[32m$1\e[0m"
@@ -11,93 +12,75 @@ printRed() {
 FILE=~/.local/share/nvim/nvim.appimage
 NVIM_FOLDER=~/.local/bin/nvim
 
-printGreen "Install Rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "$HOME/.cargo/env"
+# printGreen "Install Rust"
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# source "$HOME/.cargo/env"
+
+printGreen "ohmyzsh Installing..."
+ohmyzsh_error=$(wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true)
+if [ -z "$ohmyzsh_error" ]; then
+  printGreen "Ohmyzsh installation successful"
+fi
 
 printGreen "Creating folders and copying files"
-cp .zshrc ~/.zshrc && mkdir -p ~/.config/nvim/ && cp -r .config/nvim/* ~/.config/nvim
+cp .zshrc ~/.zshrc && mkdir -p ~/.config/nvim/ && mkdir -p ~/.local/share/nvim && cp -r .config/nvim/* ~/.config/nvim 
 
-printGreen "Install Packer"
-git clone --depth 1 https://github.com/wbthomason/packer.nvim \
-    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-
-printGreen "Install ohmyzsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-printGreen "Install starship"
-curl -sS https://starship.rs/install.sh | sh -s -- -y
-mkdir && cp .config/starship.toml ~/.config/starship.toml
+printGreen "Starship Installing"
+for i in $(seq 1 2)
+do
+  starship_error=$(curl -sS https://starship.rs/install.sh | sh -s -- -y | grep -q 'Permission denied') 
+  printRed "$starship_error"
+  if [ -z "$starship_error" ]; then
+    printGreen "Starship installation successful"
+    printGreen "Copying startship config"
+    cp .config/starship.toml ~/.config/starship.toml
+    break
+  else
+    printGreen "Change Owner tmp folder" 
+    sudo chmod 777 /tmp/
+  fi
+done
 
 if [ -d "~/.oh-my-zsh/custom/plugins/zsh-aliases-exa/" ]; then
     printGreen "zsh plugins is exists"
 else
-    git clone https://github.com/DarrinTisdale/zsh-aliases-exa ~/.oh-my-zsh/custom/plugins
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins
+    git clone https://github.com/DarrinTisdale/zsh-aliases-exa ~/.oh-my-zsh/custom/plugins/zsh-aliases-exa
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 fi
 
-printGreen "Install EXA"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-printGreen "Install FZF"
+printGreen "FZF Installing..."
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+echo "Y Y Y" | ~/.fzf/install
 
+printGreen "NEOVIM Installing..."
+if [ -f "$FILE" ]; then
+    printRed "$FILE exists."
+else
+    printGreen "$FILE does not exits. -> download from github repo."
+    wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim.appimage -P ~/.local/share/nvim/
+    chmod u+x ~/.local/share/nvim/nvim.appimage && ~/.local/share/nvim/nvim.appimage --headless -es
+fi
 
-
-# printGreen "Install NEOVIM........"
-
-# printGreen "Create Nvim folder"
-# mkdir -p ~/.local/share/nvim
-# echo "download nvim"
-
-
-# if [ -f "$FILE" ]; then
-#     printRed "$FILE exits."
-# else
-#     printGreen "$FILE does not exits. -> download from github repo."
-#     wget https://github.com/neovim/neovim/releases/download/v0.6.0/nvim.appimage -P ~/.local/share/nvim/
-#     printGreen "Install Nvim"
-#     chmod u+x ~/.local/share/nvim/nvim.appimage && ~/.local/share/nvim/nvim.appimage
-# fi
-
-# if [ -L "$NVIM_FOLDER" ]; then
-#     printRed "Nvim Folder Already Link"
-# else
-#     printGreen "Link Folder Nvim"
-#     mkdir -p ~/.local/bin
-#     ln -s $HOME/.local/share/nvim/nvim.appimage $NVIM_FOLDER
-# fi
-
-
-
-
-# declare -r PWD=$(pwd)
-# declare -r WHOAMI=$(whoami)
-# declare -r VIM_PLUG=~/.vim/autoload/plug.vim
-
-#################
-### Setup vimrc ###
-#################
-# if $(hash vim); then
-#     if [[ ! -f $VIM_PLUG ]]; then
-#         curl  --silent -fLo $VIM_PLUG --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/main/plug.vim
-#     fi
-#     ln -fFs $(pwd)/vimrc ~/.vimrc
-#     vim +PlugUpgrade +PlugClean +PlugInstall +PlugUpdate +qall
-
-#     if $(hash go); then
-#         vim ~/.vim.go +GoInstallBinaries +GoUpdateBinaries +qall
-#     fi
-
-# else
-#     echo "Oops! We could not find vim on your system."
-# fi
-
-
-
-#Nvim Plugin
-# printGreen "Install Nvim Plugin"
-# mkdir -p ~/.config/nvim/
-# cp -r .config/nvim ~/.config/nvim/*
+if [ -L "$NVIM_FOLDER" ]; then
+    printRed "Nvim folder already linked"
+else
+    printGreen "Create link to nvim folder "
+    mkdir -p ~/.local/bin
+    ln -s $HOME/.local/share/nvim/nvim.appimage $NVIM_FOLDER
+fi
+PWD=$(pwd)
+WHOAMI=$(whoami)
+PAKCER=~/.local/share/nvim/site/pack/packer/start/packer.nvim
+##################
+###Setup PACKER###
+##################
+if nvim >/dev/null; then
+    if [[ ! -f $PAKCER ]]; then
+        printGreen "Packer Installing..."
+        git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+        ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    fi
+    nvim +PackerClean +PackerInstall +PlugUpdate +qall
+else
+    echo "Oops! We could not find neovim on your system."
+fi
