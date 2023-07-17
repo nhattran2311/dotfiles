a18c57e06a4f53a724ef9c8e99f3d4e8ee253ec1
Nhat Tran <tranminhnhat2311@gmail.com>
2023-07-17 21:50:35 +0700

    Update base.lua

--- .config/nvim/lua/hori/base.lua
+++ .config/nvim/lua/hori/base.lua
@@ -64,20 +64,4 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 -- Add asterirks in block comments
 set.formatoptions:append { 'r' }
 
--- Transparent config
-require("transparent").setup({
-  enable = false, -- boolean: enable transparent
-  extra_groups = { -- table/string: additional groups that should be cleared
-    -- In particular, when you set it to 'all', that means all available groups
-
-    -- example of akinsho/nvim-bufferline.lua
-    "BufferLineTabClose",
-    "BufferlineBufferSelected",
-    "BufferLineFill",
-    "BufferLineBackground",
-    "BufferLineSeparator",
-    "BufferLineIndicatorSelected",
-  },
-  exclude_groups = {}, -- table: groups you don't want to clear
-})
 
