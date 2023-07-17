01a6ecac5911828a5d75873da3e4fe9c77d81d98
Nhat Tran <tranminhnhat2311@gmail.com>
2023-07-17 21:51:12 +0700

    Update plugins.lua

--- .config/nvim/lua/hori/plugins.lua
+++ .config/nvim/lua/hori/plugins.lua
@@ -223,9 +223,16 @@ return require('packer').startup(function(use)
   use({
     "xiyaowong/nvim-transparent",
     config = function()
-      require("transparent").setup({
-        enable = false,
-      })
+     require("transparent").setup({
+       groups = { -- table: default groups
+         'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+         'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+         'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+         'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+       },
+       extra_groups = {}, -- table: additional groups that should be cleared
+       exclude_groups = {}, -- table: groups you don't want to clear
+     })
     end,
   })
 end)
